bdeb8a253fc9435b572024e6a6225a5323d0a69a
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2022-03-25 10:53:37 -0700

    Improve comments

--- fastapi-bot/__main__.py
+++ fastapi-bot/__main__.py
@@ -15,54 +15,68 @@ from fastapi.staticfiles import StaticFiles
 
 load_dotenv()
 
+# Create a new FastAPI instance
 app = FastAPI()
+# Mount the static directory to the app
 app.mount("/static", StaticFiles(directory="static"), name="static")
+# Create the Hikari GatewayBot instance
 bot = hikari.GatewayBot(
     token=os.environ["TOKEN"],
     cache_settings=None,
     intents=hikari.Intents.ALL_UNPRIVILEGED | hikari.Intents.GUILD_MEMBERS,
 )
 
+# Grab the guild channel and admin role from the environment
 CHANNEL = int(os.environ["CHANNEL"])
 ADMIN_ROLE_ID = int(os.environ["ADMIN_ROLE_ID"])
 
-# In a real scenario this information should be stored in a database
+# In the real world these should be stored in a database
 pending_requests: list[int] = []
 approved_members: list[int] = []
 
 
+# Starts the bot when the server starts
 @app.on_event("startup")
 async def on_startup() -> None:
     await bot.start()
 
 
+# Closes the bot when the server closes
 @app.on_event("shutdown")
 async def on_shutdown() -> None:
     await bot.close()
 
 
+# Returns the index.html page
 @app.get("/")
 async def index() -> FileResponse:
     return FileResponse("static/index.html")
 
 
+# Returns the oops.html page
 @app.get("/oops")
 async def oops() -> FileResponse:
     return FileResponse("static/oops.html")
 
 
+# Validates an incoming submission on the index page form
 @app.post("/access/request")
 async def access_request(req: Request) -> Response:
+    # Extract form data into variables
     form_data = await req.form()
     userid: int = int(form_data.get("userid"))
     github_link: str = form_data.get("github-link")
 
     if userid in pending_requests:
+        # This user already submitted their info
         return RedirectResponse(
             "/oops?You already have a request pending.", status_code=302
         )
 
+    # This user is now pending
     pending_requests.append(userid)
+
+    # Build an action row with an approve and deny buttons
     action_row = (
         bot.rest.build_action_row()
         .add_button(hikari.ButtonStyle.PRIMARY, f"{userid}-approve-access")
@@ -73,6 +87,7 @@ async def access_request(req: Request) -> Response:
         .add_to_container()
     )
 
+    # Build an embed to send to the channel
     embed = (
         hikari.Embed(
             title="New application to join",
@@ -84,7 +99,10 @@ async def access_request(req: Request) -> Response:
         .add_field("Status", "```PENDING```")
     )
 
+    # Send the embed to the channel
     await bot.rest.create_message(CHANNEL, embed, component=action_row)
+
+    # Redirect the user on the site to the thanks.html page
     return FileResponse("static/thanks.html")
 
 
@@ -106,7 +124,7 @@ async def on_interaction(event: hikari.InteractionCreateEvent) -> None:
         return None
 
     if inter.channel_id != CHANNEL:
-        # This isn't the channel we send out requests to
+        # This isn't the channel we send our requests to
         return None
 
     if inter.member and ADMIN_ROLE_ID not in inter.member.role_ids:
@@ -154,12 +172,14 @@ async def do_approve_action(inter: hikari.ComponentInteraction, userid: int) -> 
     embed.add_field("Status", f"```APPROVED by {inter.user}```")
 
     try:
+        # Try to DM the user
         dm = await bot.rest.create_dm_channel(userid)
         await dm.send(
             "Hi were happy to inform you that you application has been accepted!"
             f" You, and only you, can join with the following link\n{invite}"
         )
     except hikari.ForbiddenError:
+        # This can fail if the user has DM's closed
         # Make the embed red since we couldn't DM the user
         embed.color = hikari.Color(0xF00707)
         embed.add_field("DM failed", str(invite))
@@ -174,4 +194,5 @@ async def do_approve_action(inter: hikari.ComponentInteraction, userid: int) -> 
     approved_members.append(userid)
 
 
+# Run the webserver
 uvicorn.run(app, host="0.0.0.0")
